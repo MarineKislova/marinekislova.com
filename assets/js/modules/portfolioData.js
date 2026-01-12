5304a6e6b185be2a9f236d0709382cd7424d7a7c
Marine <marinekislova@gmail.com>
2026-01-12 14:22:40 +0200

    readme

---
+++ assets/js/modules/portfolioData.js
@@ -0,0 +1,22 @@
+const portfolio = [
+    {
+        id: "Interia",
+        title: "Project 1",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        image: "project1.jpg",
+    },
+    {
+        id: "project2",
+        title: "Project 2",
+        description: "Nullam facilisis lacus sed risus bibendum, a faucibus dolor gravida.",
+        image: "project2.jpg",
+    },
+    {
+        id: "3",
+        title: "MstersHub",
+        description: "Quisque bibendum nisi eget ipsum dictum, vel tristique ex semper.",
+        descriptionUA: "Quisque bibendum nisi eget ipsum dictum, vel tristique ex semper.",
+        descriptionRU: "Адаптивный веб-сайт с кастомной интерактивной логикой и оптимизированной производительностью. Реализованы два слайдера без использования сторонних библиотек, модальные окна, табы и аккордеоны на чистом JavaScript. Страница проходит HTML-валидацию, имеет стабильную верстку (CLS = 0) и минимальную блокировку основного потока. Итоговая оценка PageSpeed — 78, ограничена наличием внешних видео-ресурсов, загружаемых с другого домена.",
+        image: "img/portfolio/mastersHub.png",
+    }
+]
